69fce5a88958427c5c90d56bfa3384b8d65a04c9
LalaFaradilla <firidilli05@gmail.com>
2023-04-05 13:41:29 +0700

    Tugas 2 dan 3

---
+++ application/controllers/Contoh01.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh01 extends CI_Controller {
+
+
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Faradilla Nur Aisha Kamal
+            Saya tinggal di daerah Kebagusan
+             Olah raga yang saya sukai adalah basket";
+	}
+}
