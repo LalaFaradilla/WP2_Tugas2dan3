a8e5ad94c39a2bcdd8a513440907ea73367f497a
LalaFaradilla <firidilli05@gmail.com>
2023-04-20 12:53:01 +0700

    menambahkan siswa

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,9 @@ $autoload['packages'] = array('url');
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('form_validation');
+$autoload['libraries'] = array('form_validation', 'session', 
+'database');
+
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +91,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file');
+$autoload['helper'] = array('url');
 
 /*
 | -------------------------------------------------------------------
